3179ac358af5a7eab068c3afc6fc7e1e7820deb7
edgar <edgar_harutyunyan_2025@mail.ru>
2024-11-15 16:41:03 +0400

    test docker

--- index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-
-try {
-  const message = core.getInput('message');
-  console.log(`Message: ${message}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
