58bf5560e8f14367d7b82ea0c4343991aa42ed02
edgar <edgar_harutyunyan_2025@mail.ru>
2024-11-15 12:43:03 +0400

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// Пример использования функции
+let num1 = 5;
+let num2 = 7;
+
+let result = sum(num1, num2);
+console.log("Результат сложения:", result);
